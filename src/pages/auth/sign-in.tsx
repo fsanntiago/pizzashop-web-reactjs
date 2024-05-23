769f5da696957d536dc641e4046022724a159564
fsanntiago <zerofabricio@hotmail.com>
2024-05-23 20:16:04 -0300

    feat: implement hook form in sign-in page

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,14 +1,45 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInSchema = z.object({
+  email: z.string().email(),
+})
+
+type signInForm = z.infer<typeof signInSchema>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<signInForm>({
+    resolver: zodResolver(signInSchema),
+  })
+
+  async function handleSignIn(data: signInForm) {
+    toast.success('Enviamos um link de autenticação para seu e-mail', {
+      action: {
+        label: 'Reeenviar',
+        onClick: () => handleSignIn(data),
+      },
+    })
+  }
+
   return (
     <>
       <Helmet title="Login" />
       <div className="p-8">
+        <Button asChild variant="ghost" className="absolute right-8  top-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -18,12 +49,12 @@ export function SignIn() {
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input name="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
-            <Button className="w-full" type="submit">
+            <Button disabled={isSubmitting} className="w-full" type="submit">
               Acessar painel
             </Button>
           </form>
