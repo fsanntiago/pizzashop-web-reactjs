a5d4af549410474f25af3f916e4ec2b25591a4ff
fsanntiago <zerofabricio@hotmail.com>
2024-05-24 10:49:40 -0300

    feat: add dashboard page

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,89 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  {
+    date: '10/12',
+    revenue: 1200,
+  },
+  {
+    date: '11/12',
+    revenue: 1600,
+  },
+  {
+    date: '12/12',
+    revenue: 800,
+  },
+  {
+    date: '13/12',
+    revenue: 1340,
+  },
+  {
+    date: '14/12',
+    revenue: 1290,
+  },
+  {
+    date: '15/12',
+    revenue: 1760,
+  },
+  {
+    date: '16/12',
+    revenue: 670,
+  },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart style={{ fontSize: 12 }} data={data}>
+            <CartesianGrid vertical={false} className="stroke-muted" />
+            <XAxis dataKey="date" tickLine={false} axisLine={false} dy={16} />
+            <YAxis
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+              width={80}
+            />
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
