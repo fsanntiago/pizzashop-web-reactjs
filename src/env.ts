208e37bda72ce7747b2d16cd8a3ca510ff4ea08a
fsanntiago <zerofabricio@hotmail.com>
2024-06-07 13:38:16 -0300

    chore: setup msw

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
+  MODE: z.enum(['test', 'development', 'production']).default('development'),
   VITE_API_URL: z.string().url().min(1),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 })
